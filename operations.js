b1fb4f0a7848c4e58a4e97204c24e642177a1f7f
Saumay Gupta <saumaygupta03@gmail.com>
2025-02-19 23:43:21 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,18 @@
+let value = 3;
+let neg = -value;
+
+// console.log(neg);
+
+let a = 3;
+let b = 4;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a**b)
+// console.log(a%b)
+
+console.log(++a);
+console.log(b++);
+
